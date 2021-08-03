e8393577e7669fff340337dfb92aa04115fbb533
Kalzak <kalzakdev@protonmail.com>
2021-08-03 14:40:16 +0800

    Various fixes and improvements

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -2,168 +2,174 @@ pragma solidity ^0.5.0;
 
 /**
 @title Rock Paper Scissors smart contract
-@author Kalzak
+@author @kalzakdev
+@notice You can use this contract to play rock paper scissors
 */
 contract RockPaperScissors {
-	// Stores the participants for the current match
-	address[2] public participants;
-	// Stores the encoded choice of the participant (keccak256 plus participants key)
+	// Stores the players in current match
+	address[2] public players;
+
+	// Stores encoded player choices
 	bytes32[2] public encodedChoice;
-	// Stores the decoded choice of the participant (in keccak256)
+
+	// Stores players choice as an int
+	// 0 = Empty
+	// 1 = Rock
+	// 2 = Paper
+	// 3 = Scissors
 	uint[2] public decodedChoice;
-	//Encoded attempt
-	bytes32[2] public encodedAttempt;
-	
-	// The stage of the match
-	// 1 = waiting for participants to join
-	// 2 = waiting for participants to make choice
-    // 3 = waiting for participants to reveal
-	// 4 = waiting for participants to settle
-	uint8 stage;
 
+	// Enum to store the state of the match	
+	enum MatchState{Join, Choose, Reveal, Settle}
+	MatchState state;
+
+	// Constructor sets all match variables to default state
 	constructor() public{
-		stage = 1;
-		participants[0] = address(0);
-		participants[1] = address(0);
-
+		reset();
+	}
+	
+	// Only allows players to access functions
+	modifier onlyPlayers() {
+		bool isPlayer = (msg.sender == players[0] || msg.sender == players[1]);
+		require(isPlayer == true, "You are not a player in the current match");
+		_;
 	}
 
 	/**
-	@dev Returns the stage of the match
+	@notice Returns the state of the current match
+       	@return Match state where 0 = Join, 1 = Choose, 2 = Reveal, 3 = Settle
 	*/
-	function checkStage() public view returns(uint) {
-		return stage;
+	function getState() public view returns(uint) {
+		return uint(state);
 	}
 
 	/**
-	@dev Adds a user to the match
+	@notice Returns the current players in the match
+       	@return Address array of size two containing players
+	*/
+	function getPlayers() public view returns(address[2] memory) {
+		return players;
+	}
+
+	/**
+	@notice Adds the message sender to the match
 	*/
 	function join() public {
-		// Check if it is the correct stage in the match
-		require(stage == 1, "Participants have already joined");	
-		// If the first slot is open add the user
-		if(participants[0] == address(0)) {
-			participants[0] = msg.sender;
+		// Check if correct match state
+		require(state == MatchState.Join, "Match does not need players to join currently");
+		// If no players have joined match yet	
+		if(players[0] == address(0)) {
+			players[0] = msg.sender;
+		// If one players is already in match
+		} else {
+			// Players can't verse themselves
+			require(players[0] != msg.sender, "You played yourself. Wait, you can't.");
+			players[1] = msg.sender;
+			// Update the match state
+			state = MatchState.Choose;
 		}
-		// If the second slot is open
-		else if(participants[1] == address(0)) {
-			// Check if the user is trying to verse themselves
-			require(participants[1] != msg.sender, "You played yourself. Wait, you can't.");
-			participants[1] = msg.sender;
-			// Update the stage of the match
-			stage = 2;
-		}
-	}
-	
-	/**
-	@dev Returns the users that have joined the match
-	*/
-	function getParticipants() public view returns(address[2] memory) {
-		return participants;
 	}
 
 	/**
-	@dev Allows users to make their choice for the match
-   	*/
-	function submitChoice(bytes32 choice) public {
-		// Check if it is the correct stage in the match
-		require(stage == 2, "The match is not at the stage of submitting choices");
-		// Check which participant the user was and add their choice
-		if(participants[0] == msg.sender) {
+	@notice Removes message sender from match if no opponent has joined
+	*/
+	function leave() public onlyPlayers {
+		// Check if correct match state
+		require(state == MatchState.Join, "Match does not need players to join currently");
+		// Remove the player from the match
+		players[0] = address(0);
+	}
+
+	/**
+	@notice Accepts and saves the players encoded choices
+       	@param choice is keccak256 encoded user choice in the format of (choice, key)
+	*/
+	function submitChoice(bytes32 choice) public onlyPlayers {
+		// Check if correct match state
+		require(state == MatchState.Choose, "Match does not need players to choose currently");
+		// Store the users encoded choice
+		if(players[0] == msg.sender) {
 			encodedChoice[0] = choice;
-		}else if(participants[1] == msg.sender) {
+		} else {
 			encodedChoice[1] = choice;
-		}else{
-			revert("You are not a participant.");
 		}
-		// If both encodedChoices are non-empty then go to next stage
+		// If both users have submitted their choices then go to next match state
 		if(encodedChoice[0] != 0 && encodedChoice[1] != 0) {
-			// Update the stage of the match
-			stage = 3;
+			// Update the match state
+			state = MatchState.Reveal;
+		}
+	}
+	
+	// Reveals a players choice
+	/**
+	@notice Reveals a players choice
+       	@param choice is the users choice, key is the key the users passed from their encoded choice
+	*/
+	function revealChoice(uint choice, string memory key) public onlyPlayers {
+		// Check if correct match state
+		require(state == MatchState.Reveal, "Match does not need players to reveal currently");
+		// Encode the values given
+		bytes32 encoded = keccak256(abi.encodePacked(choice, key));
+		// Check if the encoded values match the players previous encoded choices
+		if(players[0] == msg.sender) {
+			if(encoded == encodedChoice[0]) {
+				decodedChoice[0] = choice;
+			}
+		} else {
+			if(encoded == encodedChoice[1]) {
+				decodedChoice[1] = choice;
+			}
+		}
+		// If both players have revealed their choice
+		if(decodedChoice[0] != 0 && decodedChoice[1] != 0) {
+			// Update the match state
+			state = MatchState.Settle;
 		}
 	}
 	
 	/**
-	@dev Reveals the choice of the user
-	*/
-	function revealChoice(uint choice, string memory key) public {
-		// Check if the reveal is done by any of the users involved
-		bytes32 encoded = keccak256(abi.encodePacked(choice, key));
-		if(participants[0] == msg.sender) {
-			encodedAttempt[0] = encoded;
-			if(keccak256(abi.encodePacked(choice, key)) == encodedChoice[0]) {
-				decodedChoice[0] = choice;
-			}	
-		}		
-		else if(participants[1] == msg.sender) {
-			encodedAttempt[1] = encoded;
-			if(keccak256(abi.encodePacked(choice, key)) == encodedChoice[1]) {
-				decodedChoice[1] = choice;
-			}	
-		}		
-		else {
-			revert("You are not a participant.");
-		}
-		// If both decodedChoices are non-empty then go to the next stage
-		if(keccak256(abi.encodePacked(decodedChoice[0])) != keccak256(abi.encodePacked('')) && keccak256(abi.encodePacked(decodedChoice[1])) != keccak256(abi.encodePacked(''))) {
-			// Update the stage of the match
-			stage = 4;
-		}
-	}
-
-	/**
-   	@dev Settles the match and resets all the variables in preparation for the next match
-	*/
-	function settle() public returns(address) {
-		uint user0Choice = decodedChoice[0];
-		uint user1Choice = decodedChoice[1];
-		address winner;
-		// If draw
-		if(user0Choice == user1Choice) {
-			winner = address(0);
-		// If u0 rock
-		}else if(user0Choice == 1) {
-			// If u1 paper
-			if(user1Choice == 2) {
-				// u1 win
-				winner = participants[1];
-			}else{
-			// Else u1 scissors
-				winner = participants[0];
-			}
-		}
-		// If u0 paper
-		else if(user0Choice == 1) {
-			// If u1 scissors
-			if(user1Choice == 3) {
-				// u1 win
-				winner = participants[1];
-			}else{
-			// Else u1 rock
-				winner = participants[0];
-			}
-		// If u0 scissors
-		}else if(user0Choice == 1) {
-			// If u1 rock
-			if(user1Choice == 1) {
-				// u1 win
-				winner = participants[1];
-			}else{
-			// Else u1 paper
-				winner = participants[0];
+	@notice Decides a winner and resets the match state
+	*/	  
+       	function settle() public onlyPlayers returns(address) {
+		require(state == MatchState.Settle, "Match does not need players to settle currently");
+		uint p1Choice = decodedChoice[0];
+		uint p2Choice = decodedChoice[1];
+		if(p1Choice == p2Choice) {
+			draw();
+		} else {
+			if((p1Choice - 1) == p2Choice % 3) {
+				win(players[0]);
+			} else {
+				win(players[1]);
 			}
 		}
 		reset();
-		return winner;
 	}
 
+	/**
+	@notice Resets all match varaibles for the next match
+	*/
 	function reset() internal {
-		participants[0] = address(0);	
-		participants[1] = address(0);
+		players[0] = address(0);	
+		players[1] = address(0);
 		encodedChoice[0] = 0;
 		encodedChoice[1] = 0;
 		decodedChoice[0] = 0;
 		decodedChoice[1] = 0;
-		stage = 1;
+		state = MatchState.Join;
 	}
+
+	/**
+	@notice Declares the winner
+	*/
+	function win(address winner) internal returns(bool){
+		return true;
+	}
+
+	/**
+	@notice Declares a draw
+	*/
+	function draw() internal returns(bool) {
+		return true;
+	}	
 }
